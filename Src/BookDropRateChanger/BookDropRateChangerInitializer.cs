a3c0736b5e6eed11106ea155d7cbe628b9d56fcc
Tana <finaltruth.shintai@gmail.com>
2022-05-30 01:10:36 +0900

    必要なライブラリ参照を追加。

---
+++ Src/BookDropRateChanger/BookDropRateChangerInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using HarmonyLib;
+
+namespace BookDropRateChanger
+{
+    public class BookDropRateChangerInitializer : ModInitializer
+    {
+        public override void OnInitializeMod()
+        {
+            try
+            {
+                var harmony = new Harmony("BookDropRateChanger");
+                harmony.PatchAll();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
